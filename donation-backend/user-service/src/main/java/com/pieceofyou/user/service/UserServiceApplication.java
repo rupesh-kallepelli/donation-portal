d67b567e46d1a0ea78c565bb4629f7b37cafd92a
rupesh_kallepelli <kallepallirupesh@gmail.com>
2025-09-20 19:07:57 +0530

    updated routes

--- donation-backend/user-service/src/main/java/com/pieceofyou/user/service/UserServiceApplication.java
+++ donation-backend/user-service/src/main/java/com/pieceofyou/user/service/UserServiceApplication.java
@@ -1,8 +1,12 @@
+
 package com.pieceofyou.user.service;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/users")})
 @SpringBootApplication
 public class UserServiceApplication {
 
